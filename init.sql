a97fe993356dd626a27537e1af0ddcab8b788a87
asish <beingasishkumar@gmail.com>
2025-08-21 12:00:00 +0530

    add base infrastructure

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE product_db;
+CREATE DATABASE order_db;
+CREATE DATABASE inventory_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE shipping_db;
+CREATE DATABASE review_db;
